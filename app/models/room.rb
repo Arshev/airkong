afd96f390ad731cd794d5b20c59800123d1bde42
Artem Shevchenko <ashev08@gmail.com>
2017-09-08 19:57:12 +0200

    Add Calendar functions and views

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,12 @@
 class Room < ApplicationRecord
 
   enum instant: {Request: 0, Instant: 1}
-  
+
   belongs_to :user
   has_many :photos
   has_many :reservations
   has_many :guest_reviews
+  has_many :calendars
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
