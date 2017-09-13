c88100fccd4c648c2acec38732af7aba0bd6b2eb
Artem Shevchenko <ashev08@gmail.com>
2017-09-13 11:58:58 +0200

    Add Notifications functions

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,7 @@ Rails.application.routes.draw do
 
   get '/notification_settings' => 'settings#edit'
   post '/notification_settings' => 'settings#update'
+  get '/notifications' => 'notifications#index'
 
   mount ActionCable.server => '/cable'
 end
